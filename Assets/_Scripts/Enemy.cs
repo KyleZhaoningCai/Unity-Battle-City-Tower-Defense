4965c660fd62e69b0b73c5ec6771f86e7f52d6e7
Zhaoning Cai <caizhaoning@gmail.com>
2020-03-21 01:53:28 -0400

    Added enemy move logic; Added player base

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int speed;
+
+    private GameObject[] waypoints;
+    private GameObject finalWaypoint;
+    private int waypointIndex;
+    private Vector2 currentWaypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypointIndex = 0;
+        currentWaypoint = waypoints[waypointIndex].transform.position;
+        RotateTowards(currentWaypoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToPosition(currentWaypoint);
+        if (Vector2.Distance(transform.position, currentWaypoint) < 0.01 && waypointIndex < waypoints.Length - 1)
+        {
+            waypointIndex++;
+            currentWaypoint = waypoints[waypointIndex].transform.position;
+            RotateTowards(currentWaypoint);
+        }
+        else if (Vector2.Distance(transform.position, currentWaypoint) < 0.01 && waypointIndex == waypoints.Length - 1)
+        {
+            currentWaypoint = finalWaypoint.transform.position;
+            RotateTowards(currentWaypoint);
+        }
+    }
+
+    public void SetWaypoints(GameObject[] waypointObjects)
+    {
+        waypoints = waypointObjects;
+    }
+
+    public void SetFinalWaypoint(GameObject finalWaypointGameObject)
+    {
+        finalWaypoint = finalWaypointGameObject;
+    }
+
+    private void MoveToPosition(Vector2 position)
+    {
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), position, speed * Time.deltaTime);
+    }
+
+    // Code from Unity Forum
+    private void RotateTowards(Vector2 target)
+    {
+        var offset = 270f;
+        Vector2 direction = target - (Vector2)transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
+    }
+}
--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+    // Global Variables
+    public int baseWallHp;
+    public string gameState;
+    public GameObject[] waypoints;
+    public GameObject[] finalWaypoints;
+    public float waveInterval;
+    public float spawnInterval;
+    public GameObject[] EnemyTypes;
+    public GameObject spawnLocation;
+
+    private int enemiesPerWave;
+    private int waveNumber;
+    private float originalWaveInterval;
+    private float originalSpawnInterval;
+    private int spawnIndex;
+    private GameObject[] enemies;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemiesPerWave = 7;
+        waveNumber = 0;
+        originalWaveInterval = waveInterval;
+        originalSpawnInterval = spawnInterval;
+        spawnInterval = 0;
+        spawnIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        waveInterval -= Time.deltaTime;
+        if (waveInterval <= 0)
+        {
+            enemies = new GameObject [enemiesPerWave];
+            spawnInterval -= Time.deltaTime;
+            if (spawnInterval <= 0 && spawnIndex < enemiesPerWave)
+            {
+                GameObject enemy = Instantiate(EnemyTypes[waveNumber], new Vector2(spawnLocation.transform.position.x, spawnLocation.transform.position.y), Quaternion.identity);
+                enemy.GetComponent<Enemy>().SetFinalWaypoint(finalWaypoints[spawnIndex]);
+                enemy.GetComponent<Enemy>().SetWaypoints(waypoints);
+                enemies[spawnIndex] = enemy;
+                spawnIndex++;
+                spawnInterval = originalSpawnInterval;
+            }
+        }
     }
 }
